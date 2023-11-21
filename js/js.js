6868b625f4e993e1f99c26d69d02fd7221a5a036
Alexander Söderwall <alexander.soderwall@gmail.com>
2023-11-21 12:24:29 +0100

    JS function to grab JSON info completed

--- js/js.js
+++ js/js.js
@@ -1,16 +1,37 @@
-const test = document.getElementById("CVAttachment");
+const CVContainer = document.getElementById("CvAttachment");
+const CVTextContainer = document.getElementById("CvTextContainer");
 
 async function getCV() {
   const response = await fetch("../json/json.json");
   if (response.ok) {
     const json = await response.json();
+    //Adds the header in the outside parent div of the article
+    let CVHeader = document.createElement(json.CV[0].Element);
+    CVHeader.textContent = json.CV[0].Text;
+    CVContainer.appendChild(CVHeader);
+    CVContainer.appendChild(CVTextContainer);
     //Loops based on how many objects there are in the json file.
-    for (i = 0; i < Object.keys(json.CV).length; i++) {
-      let testElement = document.createElement(json.CV[i].Element);
-      testElement.textContent = "test";
-      test.appendChild(testElement);
-      console.log(json.CV[i].Element);
+    for (i = 1; i < Object.keys(json.CV).length; i++) {
+      //Creates specified element and adds its text content
+      let CVElements = document.createElement(json.CV[i].Element);
+      CVElements.textContent = json.CV[i].Text;
+      //Checks if the element has a class and adds it
+      if (json.CV[i].Class.length > 0) {
+        CVElements.classList.add(json.CV[i].Class);
+      }
+      //Checks if a linebreak is needed
+      if (json.CV[i].LineBreaks > 0) {
+        for (j = 0; j < json.CV[i].LineBreaks; j++) {
+          let lineBreaks = document.createElement("br");
+          CVTextContainer.appendChild(lineBreaks);
+        }
+      }
+      //Appends the text to the relevant container
+      CVTextContainer.appendChild(CVElements);
     }
+    //adds one final linebreak once finished
+    let finalLineBreak = document.createElement("br");
+    CVTextContainer.appendChild(finalLineBreak);
   } else {
     console.log("fail");
   }
